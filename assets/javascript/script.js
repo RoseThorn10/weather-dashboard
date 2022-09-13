c6bf2b49a3c4f75ed679ed04b1825dc0add4a131
RoseThorn10 <lrroman@comcast.net>
2022-09-12 20:14:29 -0500

    Modifying script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
     const APIKey = "eca2c2ddac9ef4066cbc3fbee74ff621";
     const baseURL = "https://api.openweathermap.org/data/2.5/weather?";
 
+    // When search is clicked get input from #searchText
     $("#searchBtn").click(function(event) {
         console.log("Clicked");
 
         var citySearch = $("#searchText").val();
         console.log(citySearch);
+
+        getWeather(citySearch);
 
         event.preventDefault(); 
 
@@ -22,19 +25,27 @@ $(document).ready(function() {
             return;
         }
 
-        var url = `$[baseURL]$[citySearch]&APPID$[APIKey]`;
+        var url = `${baseURL}q=${city}&APPID=${APIKey}`;
+
+        console.log(url);
 
         fetch(url)
         .then(function (response) {
             if (response.ok) {
-                console.log(response);
                 response.json().then(function (data) {
-                    
+
+                    console.log(JSON.stringify(data));
+
+
                 })
+            } else {
+                console.log("Not OK");
             }
-        })
+        }).catch(function(error) {
+            console.log("Error connecting");
+        });
+
+
     }
 
-
-
-})
+});
